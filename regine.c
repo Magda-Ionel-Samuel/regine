09c5cc13f99040fa412b42e06fa8516e7927ae53
Magda Ionel <148451155+Magda-Ionel-Samuel@users.noreply.github.com>
2023-11-30 23:31:21 +0200

    Update regine.c

--- regine.c
+++ regine.c
@@ -32,3 +32,48 @@ int Valid(k) {
 	}
 	return 1;
 }
+void Print() {
+	int i, j;
+	for (i = 1; i <= n; i++) {
+		for (j = 1; j <= n; j++)
+		{
+			if (v[i] == j)
+				printf("R ");
+			else
+				printf("0 ");
+		}
+		printf("\n");
+	}
+	printf("\n");
+}
+void Back(int n) {
+	k = 1;
+	Init(k);
+	while (k > 0) {
+		int isS = 0;
+		int isV = 0;
+		if (k <= n)
+			do {
+				isS = Succesor(k);
+				if (isS)
+					isV = Valid(k);
+			} while (isS && !isV);
+			if (isS)
+				if (Solution(k))
+					Print();
+				else {
+					k++; Init(k);
+				}
+			else
+				k = k - 1;
+	}
+}
+
+int main()
+{
+
+	printf("Introduceti n: "); scanf("%d", &n);
+	Back(n);
+	system("pause");
+	return 0;
+}
