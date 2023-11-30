da91a3e40756fa681734559c0cc4dd573aab2199
Magda Ionel <148451155+Magda-Ionel-Samuel@users.noreply.github.com>
2023-11-30 23:29:46 +0200

    Update regine.c

--- regine.c
+++ regine.c
@@ -3,3 +3,32 @@
 
 int v[10];
 int k, n;
+void Init(k) {
+	v[k] = 0;
+}
+
+
+int Succesor(k) {
+	if (v[k] < n) {
+		v[k]++;
+		return 1;
+	}
+	else
+		return 0;
+}
+
+
+int Solution(int k) {
+	return (k == n);
+}
+
+int Valid(k) {
+	int i;
+	for (i = 1; i <= k - 1; i++)
+	{
+		if ((v[i] == v[k]) || (abs((v[k] - v[i])) == (k - i)))
+			return 0;
+
+	}
+	return 1;
+}
